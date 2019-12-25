aa6706689015a123191360603c11ec1c7b4811cc
jasimdipu <dipu.j247@gmail.com>
2019-12-25 21:53:07 +0600

    refactoring to commoncrud interface

---
+++ src/main/java/com/stornit/petsclinic/services/CrudService.java
@@ -0,0 +1,11 @@
+package com.stornit.petsclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+    T findById(ID id);
+    T save(T object);
+    void delete(T object);
+    void deleteById(T id);
+}
